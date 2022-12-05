a91028a9ff6b0c3758ce3386a22d4f2bb87ce85b
ShaneKolkoto <shanekolkoto@gmail.com>
2022-12-05 11:39:18 +0200

    added betty

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """a function that replaces an element in a list at a specific position without modifying the original list"""
 
-
 def new_in_list(my_list, idx, element):
     length = len(my_list)
 
