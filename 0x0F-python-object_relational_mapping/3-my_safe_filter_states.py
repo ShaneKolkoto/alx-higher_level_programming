afdaf445f3c3d362bf66db03b1a49610ebe0d933
ShaneKolkoto <shanekolkoto@gmail.com>
2023-03-18 23:09:13 +0200

    fixed and updated

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-Displays all values in the states table of the database hbtn_0e_0_usa
-whose name matches that supplied as argument.
-Safe from SQL injections.
-Usage: ./3-my_safe_filter_states.py <mysql username> \
-                                    <mysql password> \
-                                    <database name> \
-                                    <state name searched>
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                   (argv[4],))
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
