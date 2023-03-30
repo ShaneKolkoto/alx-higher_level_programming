206e9ce0fa6129442ddfb9b832e9aa7920cc6c56
ShaneKolkoto <shanekolkoto@gmail.com>
2023-03-31 00:25:03 +0200

    fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,20 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers.
-
-    Args:
-        list_of_integers (list): List of unsorted integers.
-
-    Returns:
-        int: A peak in the list.
-    """
-
-    # Check if the list is empty
-    if not list_of_integers:
+    n = len(list_of_integers)
+    if n == 0:
         return None
-
-    # Find the index of the middle element
-    mid_index = len(list_of_integers) // 2
-
-    # Compare the middle element with its adjacent elements
-    if (mid_index == 0 or list_of_integers[mid_index - 1] <= list_of_integers[mid_index]) and (mid_index == len(list_of_integers) - 1 or list_of_integers[mid_index + 1] <= list_of_integers[mid_index]):
-        # If the middle element is a peak, return it
-        return list_of_integers[mid_index]
-    elif mid_index > 0 and list_of_integers[mid_index - 1] > list_of_integers[mid_index]:
-        # If the left adjacent element is greater than the middle element, search for a peak in the left half
-        return find_peak(list_of_integers[:mid_index])
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+    mid = n // 2
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and \
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        # If the right adjacent element is greater than the middle element, search for a peak in the right half
-        return find_peak(list_of_integers[mid_index + 1:])
+        return find_peak(list_of_integers[mid + 1:])
