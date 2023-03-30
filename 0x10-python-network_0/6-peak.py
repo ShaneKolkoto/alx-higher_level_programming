02ca168d520467ed1f7dc2c42b7f93e2ce52e37f
ShaneKolkoto <shanekolkoto@gmail.com>
2023-03-31 00:23:28 +0200

    fixed code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,28 @@
-#!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
-
-
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
+    """
+    Finds a peak in a list of unsorted integers.
 
-    if list_of_integers is None or list_of_integers == []:
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Returns:
+        int: A peak in the list.
+    """
+
+    # Check if the list is empty
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    # Find the index of the middle element
+    mid_index = len(list_of_integers) // 2
+
+    # Compare the middle element with its adjacent elements
+    if (mid_index == 0 or list_of_integers[mid_index - 1] <= list_of_integers[mid_index]) and (mid_index == len(list_of_integers) - 1 or list_of_integers[mid_index + 1] <= list_of_integers[mid_index]):
+        # If the middle element is a peak, return it
+        return list_of_integers[mid_index]
+    elif mid_index > 0 and list_of_integers[mid_index - 1] > list_of_integers[mid_index]:
+        # If the left adjacent element is greater than the middle element, search for a peak in the left half
+        return find_peak(list_of_integers[:mid_index])
+    else:
+        # If the right adjacent element is greater than the middle element, search for a peak in the right half
+        return find_peak(list_of_integers[mid_index + 1:])
