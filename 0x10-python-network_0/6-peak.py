b66ed44fd30cbaa305745661897cd2af654c3c6a
ShaneKolkoto <shanekolkoto@gmail.com>
2023-03-31 00:27:10 +0200

    fixed 1 fali checker

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""
+Find Peak task 6
+"""
 
 
 def find_peak(list_of_integers):
-    n = len(list_of_integers)
-    if n == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    if n == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    if n == 2:
+    elif size == 2:
         return max(list_of_integers)
-    mid = n // 2
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and \
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if list_of_integers[mid] < list_of_integers[mid - 1]:
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
